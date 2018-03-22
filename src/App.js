48df9c29793766a2d6189288ed2332a055b771de
Justin Hays <jhays124@yahoo.com>
2018-03-22 13:36:09 -0700

    what are we doing? we are about to handle token exchange

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import LoginForm from './components/LoginForm'
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3000/'
 
 import './App.css'
 
 class App extends Component {
 
   componentDidMount() {
-    console.log(window.location.search);
+    console.log(window.location.search)
+    this.handleTokenExchange(window.location.search)
+  }
+
+  handleTokenExchange = (tokenStr) => {
+    //no tokenStr? stahp!
+    if (!tokenStr) return null
+    //yes tokenStr? sally forth!
+    axios.post(`${baseURL}auth${tokenStr}`)
+      .then(data => {
+        //what our BE returns should be.... an obj of { access_token, scope }
+        localStorage.setItem('token', data.access_token)
+      })
   }
 
   auth = () => {
     let nonce = this.makeNonce()
     localStorage.setItem('State', nonce)
     let clientID = 'cd8ff7558bb5bb6a0d6a'
-    let reqParams = `client_id=${clientID}&redirect_uri=http://commit-m.surge.sh&scope=read:user%20repo&state=${nonce}&allow_signup=true`
+    let reqParams = `client_id=${clientID}&redirect_uri=http://commit-m.surge.sh/&scope=read:user%20repo&state=${nonce}&allow_signup=true`
 
     window.location.replace(`https://github.com/login/oauth/authorize?${reqParams}`)
   }
