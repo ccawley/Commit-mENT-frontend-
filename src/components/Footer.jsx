8dc7174ff48e50184757d43a79a526676d5be1aa
Curtis Cawley <curtis.cawley@icloud.com>
2018-03-29 10:07:00 -0700

    minor change to footer, still not sticking to bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
   return (
     <div onBottom className='footer' textAlign='center'>
       <Responsive {...Responsive.onlyComputer}>
-        <p>Commit-m ~ &copy; 2017</p>
+        <p>Commit-m ~ 2017</p>
       </Responsive>
       <Responsive {...Responsive.onlyTablet}>
-        <p>Commit-m ~ &copy; 2017</p>
+        <p>Commit-m ~ 2017</p>
       </Responsive>
       <Responsive {...Responsive.onlyMobile}>
-        <p>Commit-m ~ &copy; 2017</p>
+        <p>Commit-m ~ 2017</p>
       </Responsive>
     </div>
   )
